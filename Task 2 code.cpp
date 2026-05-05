eddd45fbf9486795228c87dc180cb7968afd1a12
Eshaan <e.v.firake1-25@student.lboro.ac.uk>
2026-05-05 21:59:30 +0100

    create apply_power_mode and fi bugs

--- Task 2 code.cpp
+++ Task 2 code.cpp
@@ -4,13 +4,14 @@
 const int B = 4275000; // B value of the thermistor
 const int R0 = 100000; // R0 = 100k
 const int pinTempSensor = A0; // Grove - Temperature Sensor connect to A0
-float temperatureData[180]; // Array to store temperature data for 3 minutes
-float real[180];
-float imag[180];
-float magnitude[180];
+const int MAX_READINGS = 180;
+float temperatureData[MAX_READINGS]; // Array to store temperature data for 3 minutes
+int samplingDelay = 1000; // 1 second delay for sampling
+float real[MAX_READINGS];
+float imag[MAX_READINGS];
+float magnitude[MAX_READINGS];
 int currentMode = 0; // 0 = ACTIVE, 1 = IDLE, 2 = POWER_DOWN
 float samplingFrequency = 1.0; // 1 sample per second
-const int MAX_READINGS = 180;
 int amountOfReadings = 180;
 
 void setup()
@@ -21,9 +22,9 @@ void setup()
 void loop()
 {
   collect_temperature_data();
-  print_temperature_data();
-  apply_dft();
   send_data_to_pc();
+  currentMode = decide_power_mode(samplingFrequency);
+  apply_power_mode();
 } 
 
 void collect_temperature_data(){
@@ -33,7 +34,7 @@ void collect_temperature_data(){
         R = R0*R;
         float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15;
         temperatureData[i] = temperature;
-        delay(1000);
+        delay(samplingDelay);
     }
    }
 
@@ -49,27 +50,27 @@ void collect_temperature_data(){
    }
 
    float* apply_dft(){
-    for (int k = 0; k<180; k++){
+    for (int k = 0; k<amountOfReadings; k++){
       real[k] = 0;
       imag[k] = 0;
-      for (int n = 0; n<180; n++){
-        real[k] += temperatureData[n] * cos(2*M_PI*k*n/180);
-        imag[k] -= temperatureData[n] * sin(2*M_PI*k*n/180);
+      for (int n = 0; n<amountOfReadings; n++){
+        real[k] += temperatureData[n] * cos(2*M_PI*k*n/amountOfReadings);
+        imag[k] -= temperatureData[n] * sin(2*M_PI*k*n/amountOfReadings);
       }
       magnitude[k] = sqrt(real[k]*real[k] + imag[k]*imag[k]);
     }
 
     // Calculate frequency using Eq. 3.2
-    static float frequency[180];
-    for (int k = 0; k<180; k++){
-      frequency[k] = (k * samplingFrequency)/180;
+    static float frequency[MAX_READINGS];
+    for (int k = 0; k<amountOfReadings; k++){
+      frequency[k] = (k * samplingFrequency)/amountOfReadings;
     }
     return frequency;
    }
 
 void send_data_to_pc(){
   float* frequency = apply_dft();
-  for (int i = 0; i<180; i++){
+  for (int i = 0; i<amountOfReadings;i++){
     Serial.print("Time: ");
     Serial.print(i);
     Serial.print(", Temp: ");
@@ -82,13 +83,28 @@ void send_data_to_pc(){
 }
 
 int decide_power_mode(float averageFrequency){
-    if(averageFrequency >){
+    if(averageFrequency > 0.5){
         return 0; // ACTIVE
     }
-    else if(averageFrequency > 1 && averageFrequency <= 0.5){
+    else if(averageFrequency > 0.1 && averageFrequency <= 0.5){
         return 1; // IDLE
     }
     else{
         return 2; // POWER_DOWN
+    }
+}
+
+void apply_power_mode(){
+    if(currentMode == 0){ // ACTIVE
+        amountOfReadings = 180; // set number of readings
+        samplingDelay = 1000; // set delay
+    }
+    else if(currentMode == 1){ // IDLE
+        amountOfReadings = 36;
+        samplingDelay = 5000;
+    }
+    else{ // POWER_DOWN
+        amountOfReadings = 6;
+        samplingDelay = 30000;
     }
 }

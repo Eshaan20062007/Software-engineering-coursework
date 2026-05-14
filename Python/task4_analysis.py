0af7df44aa38f286e0ca5a9ca05d1fe3ce37780e
Eshaan <e.v.firake1-25@student.lboro.ac.uk>
2026-05-14 14:26:49 +0100

    create task 4 file and add CSV file reading and data loading

---
+++ Python/task4_analysis.py
@@ -0,0 +1,27 @@
+# task4_analysis.py
+# Task 4 - Temperature Data Analysis and Visualisation
+
+import matplotlib.pyplot as plt
+
+# Load the data form the CSV file
+# Create empty lists to store the data
+time_values = []
+temperature_values = []
+
+# Open the CSV file and read it line by line
+with open(r'C:\Users\evfir\OneDrive - Loughborough University\Visual studio code\Software-engineering-coursework\arduino\temperature_data.csv', 'r') as file:
+    for line in file:
+
+        # Split by comma to separate time and temperature
+        parts = line.strip().split(',')
+
+        # Extract the number after "time: "
+        time = float(parts[0].split(':')[1])
+
+        # Extract the number after "temperature: "
+        temperature = float(parts[1].split(':')[1])
+
+        time_values.append(time)
+        temperature_values.append(temperature)
+
+print(f"Loaded {len(time_values)} readings")
